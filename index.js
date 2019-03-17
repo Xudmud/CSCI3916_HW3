88362a81be6e67a58dfbafadcb9505b565951b1b
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 16:31:09 -0600

    Changed "user already exists" error code to not be 200.

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ router.post('/signup', function(req, res) {
             if (err) {
                 // duplicate entry
                 if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists. '});
+                    return res.status(409).send({ success: false, message: 'A user with that username already exists. '});
                 else
                     return res.send(err);
             }
