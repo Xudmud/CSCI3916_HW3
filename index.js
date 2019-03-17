d686db0bc2002354a200d7d377f0a9c9a43f90ef
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 12:24:34 -0600

    More required npm modules

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var authJwtController = require('./auth_jwt');
+var User = require('./Users');
+var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
-
