f9174ac6cc6f4e2a46373d159d95e108e8d8e46e
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 23:50:08 -0600

    Remove the DELETE check temporarily

--- index.js
+++ index.js
@@ -147,8 +147,8 @@ router.route('/movies')
         //How would you "update" the movie data...
         //Send a valid title, then updated information?
         Movie.findOne({ title: searchtitle }).select('title year genre actor').exec(function(err, movie) {
-            //if(movie === null)
-                //return(res.status(404).send({success: false, msg: 'Movie not found.'}));
+            if(movie === null)
+                return(res.status(404).send({success: false, msg: 'Movie not found.'}));
             console.log("Checking year");
             if(req.body.year)
                 movie.year = req.body.year;
@@ -182,10 +182,10 @@ router.route('/movies')
         //Receives: title to be deleted.
         //Weakness: This will find the first instance and delete that.
         //First check if the movie even exists.
-        Movie.findOne({ title: req.body.title }).select('title').exec(function(err, movie) {
+        /*Movie.findOne({ title: req.body.title }).select('title').exec(function(err, movie) {
             if(movie === null)
                 return(res.status(404).send({success: false, msg: 'Movie not found.'}));
-            });
+            });*/
         //If so, delete it, throw an error if it fails.
         Movie.deleteOne({ title: req.body.title });
         res.json({success: true, msg: 'Successfully deleted movie.'});
