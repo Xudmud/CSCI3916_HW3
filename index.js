5bd4013073f1442d320d7a74a3c47024ece1a1d3
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 20:51:18 -0600

    array.length isn't a method in javascript

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ router.route('/signin')
 router.route('/movies')
     .post(authJwtController.isAuthenticated, function (req, res) {
         console.log(req.body);
-        if(!req.body.title || !req.body.year || req.body.actor.length() != 3)
+        if(!req.body.title || !req.body.year || req.body.actor.length != 3)
             res.json({success: false, msg: 'Please include all required fields!'});
         else {
             var movieNew = new Movie();
