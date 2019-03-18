531c119b5e3bd2871c7132cab45e1699aa9b5f6d
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 20:32:29 -0600

    Fixed typing on year

--- Movies.js
+++ Movies.js
@@ -10,14 +10,10 @@ mongoose.set('useCreateIndex', true);
 //Movie schema
 var MovieSchema = new Schema({
     title: {type: String, required: true},
-    year: {type: Int, required: true},
+    year: {type: Num, required: true},
     genre: {type: String, required: true},
     actor: {type: String, type: String, required: true}
 });
 
-MovieSchema.pre('save', function(next) {
-    var movie = this;
-
-})
 
 module.exports = mongoose.model('Movie', MovieSchema);
