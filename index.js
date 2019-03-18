59fa156eefeb3139b8cf0fb9801730f813737c28
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 23:55:54 -0600

    Added braces to if-statements

--- index.js
+++ index.js
@@ -147,21 +147,21 @@ router.route('/movies')
         //How would you "update" the movie data...
         //Send a valid title, then updated information?
         Movie.findOne({ title: searchtitle }).select('title year genre actor').exec(function(err, movie) {
-            if(movie === null)
+            if(movie === null) {
                 return(res.status(404).send({success: false, msg: 'Movie not found.'}));
+            }
             console.log("Checking year");
-            if(req.body.year)
-                movie.year = req.body.year;
+            if(req.body.year) movie.year = req.body.year;
             console.log("Checking genre");
-            if(req.body.genre)
-                movie.genre = req.body.genre;
+            if(req.body.genre) movie.genre = req.body.genre;
             console.log("Checking actor array");
-            if(req.body.actor)
+            if(req.body.actor) {
                 if(req.body.actor.length != 3){
                     return(res.status(412).send({success: false, msg: 'Please use three actors.'}));
                 } else {
                     movie.actor = req.body.actor;
                 }
+            }
 
             console.log("Movie built, sending to update.");
             Movie.update(
