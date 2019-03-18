47f9bf33032b883495a7ecab8c05f3011ca05183
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 20:18:15 -0600

    Fix syntax error

--- Movies.js
+++ Movies.js
@@ -2,9 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //Don't need bcrypt really
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
+
+//Movie schema
 var MovieSchema = new Schema({
     title: {type: String, required: true},
     year: {type: Int, required: true},
     genre: {type: String, required: true},
-    actor: {{type: String, type: String}, required: true}
-})
+    actor: {type: String, type: String, required: true}
+});
+
+MovieSchema.pre('save', function(next))
