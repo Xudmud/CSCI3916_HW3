01a49d6e2bd10fcda61fe35eb1e08b465f79e914
Xudmud <morichikarinnosuke17@gmail.com>
2019-03-17 23:26:05 -0600

    Is it because I'm using findOne?

--- index.js
+++ index.js
@@ -179,10 +179,10 @@ router.route('/movies')
         //Receives: title to be deleted.
         //Weakness: This will find the first instance and delete that.
         //First check if the movie even exists.
-        Movie.findOne({ title: req.body.title}).select('title').exec(function(err, movie) {
+        /*Movie.findOne({ title: req.body.title}).select('title').exec(function(err, movie) {
             if(movie === null)
                 return(res.status(404).send({success: false, msg: 'Movie not found.'}));
-            });
+            });*/
         //If so, delete it, throw an error if it fails.
         Movie.deleteOne({title: req.body.title});
         res.json({success: true, msg: 'Successfully deleted movie.'});
